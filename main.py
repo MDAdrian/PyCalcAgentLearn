77c85e556495df9c754d4f5f18888e767bd19906
MDAdrian <adi.maniu33@gmail.com>
2025-10-16 21:23:20 +0300

    CLI

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 import os
+import sys
+import click
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
-def main():
+@click.command()
+@click.argument("prompt", type=str)
+@click.option("--verbose", is_flag=True, help="Enable verbose output")
+def main(prompt, verbose):
     print("Hello from pyagent!")
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=messages
     )
+    
+    if verbose:
+        print(f"User prompt: {prompt}")
+        print(f" * Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f" * Response tokens: {response.usage_metadata.candidates_token_count}\n")
     print(f"Response: \n{response.text}")
-    print(f" * Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f" * Response tokens: {response.usage_metadata.candidates_token_count}\n")
 
 
 if __name__ == "__main__":
