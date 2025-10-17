4f2baad7e6a2c54d05f68846658397714fe9dc9c
MDAdrian <adi.maniu33@gmail.com>
2025-10-17 21:29:28 +0300

    prompt

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,8 +1,28 @@
 import os
-
+from google.genai import types
 from config import MAX_CHARS
 
     
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+
 def get_files_info(working_directory, directory="."):
     """
     List files in a directory relative to a working directory, safely.
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from functions.get_files_info import available_functions
+
 
 @click.command()
 @click.argument("prompt", type=str)
@@ -20,15 +22,28 @@ def main(prompt, verbose):
         types.Content(role="user", parts=[types.Part(text=prompt)]),
     ]
 
+    system_prompt = """
+        You are a helpful AI coding agent.
+
+        When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+        - List files and directories
+
+        All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
+
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
-        contents=messages
+        contents=messages,
+        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt
+)
     )
     
     if verbose:
         print(f"User prompt: {prompt}")
         print(f" * Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f" * Response tokens: {response.usage_metadata.candidates_token_count}\n")
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
     print(f"Response: \n{response.text}")
 
 
